55a9c57b74c4d8b119183c9712bf9834c2ee718f
Hyedenma Sey <hyedenmasey@gmail.com>
2023-10-24 15:25:43 +0200

    update

--- contracts/Pattern1.sol
+++ contracts/Pattern1.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 contract StripePattern {
-
     struct Stripe {
         uint256 positionX;
         uint256 colorHue;
@@ -11,42 +10,55 @@ contract StripePattern {
     }
 
     Stripe[20] public stripes;
+    bool public isPatternRandomized = false; // flag to check if randomizePattern was called
 
-    // Function to initialize the pattern with random values based on a token ID
     function randomizePattern(uint256 tokenId) public {
-        // Seed the random generator
         uint256 random = uint256(keccak256(abi.encodePacked(tokenId)));
-
         for (uint256 i = 0; i < stripes.length; i++) {
-            // Generate random values based on the token ID and loop iteration
             stripes[i].positionX = (random / (i + 1)) % 500;
             stripes[i].colorHue = (random / (i + 1)) % 360;
-            stripes[i].colorSaturation = 50 + (random / (i + 1)) % 50; // Range: 50-100
-            stripes[i].colorBrightness = 50 + (random / (i + 1)) % 50; // Range: 50-100
+            stripes[i].colorSaturation = 50 + (random / (i + 1)) % 50;
+            stripes[i].colorBrightness = 50 + (random / (i + 1)) % 50;
         }
+        isPatternRandomized = true; // set the flag to true after randomization
     }
 
     function getSvgData() public view returns (string memory) {
-        uint256 stripeWidth = 500 / stripes.length; // width divided by number of stripes
+        require(isPatternRandomized, "Pattern has not been randomized yet. Please call randomizePattern first.");
 
-        // Start of the SVG data
+        if(stripes.length == 0) {
+            return "Error: No stripes present.";
+        }
+
+        uint256 stripeWidth = 500 / stripes.length;
         bytes memory svg = abi.encodePacked('<svg width="500" height="500" xmlns="http://www.w3.org/2000/svg">');
 
         for (uint256 i = 0; i < stripes.length; i++) {
-            svg = abi.encodePacked(svg, '<rect x="', toString(stripes[i].positionX), 
-            '" y="0" width="', toString(stripeWidth), 
-            '" height="500" fill="hsl(', toString(stripes[i].colorHue), ', ', toString(stripes[i].colorSaturation), 
-            '%, ', toString(stripes[i].colorBrightness), '%)" />');
+            svg = abi.encodePacked(svg, 
+                    '<rect x="', toString(stripes[i].positionX), 
+                    '" y="0" width="', toString(stripeWidth), 
+                    '" height="500" fill="hsl(', toString(stripes[i].colorHue), ', ', 
+                    toString(stripes[i].colorSaturation), 
+                    '%, ', toString(stripes[i].colorBrightness), '%)" />');
         }
 
-        // End of the SVG data
         svg = abi.encodePacked(svg, '</svg>');
-
         return string(svg);
     }
 
     function toString(uint256 value) internal pure returns(string memory) {
-      // Convert a uint256 value to a string
-      return string(abi.encodePacked(value));
+        // Maximum length of uint256 is 78 digits
+        bytes memory buffer = new bytes(78);
+        uint8 length = 0;
+        while (value != 0) {
+            buffer[length++] = bytes1(uint8(48 + value % 10));
+            value /= 10;
+        }
+        // Reverse the string
+        bytes memory result = new bytes(length);
+        for (uint8 i = 0; i < length; i++) {
+            result[i] = buffer[length - 1 - i];
+        }
+        return string(result);
     }
 }
